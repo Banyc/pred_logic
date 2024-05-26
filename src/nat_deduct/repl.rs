e7a37f4fae32339da1e1b55c8c4834a1e926b7f1
Banyc <36535895+Banyc@users.noreply.github.com>
2024-05-27 00:29:33 +0800

    feat: generic DM

--- src/nat_deduct/repl.rs
+++ src/nat_deduct/repl.rs
@@ -49,6 +49,22 @@ replace! (
     two_and_not,
     two_not_or,
 );
+pub fn de_morgen(expr: &Arc<Expr>, unnamed_space: &UnnamedGen) -> Option<Arc<Expr>> {
+    match (
+        de_morgen_not_and(expr, unnamed_space),
+        de_morgen_or_not(expr, unnamed_space),
+        de_morgen_not_or(expr, unnamed_space),
+        de_morgen_and_not(expr, unnamed_space),
+    ) {
+        (Some(x), _, _, _) | // _
+        (_, Some(x), _, _) | // _
+        (_, _, Some(x), _) | // _
+        (_, _, _, Some(x)) => {
+            Some(x)
+        }
+        _ => None,
+    }
+}
 
 replace! (
     commutative_or,
@@ -73,7 +89,7 @@ mod tests {
     fn test_dm_not_and() {
         let p = named_var_expr("p");
         let q = named_var_expr("q");
-        let expr = Arc::new(two_not_and(Arc::clone(&p), Arc::clone(&q)));
+        let expr = two_not_and(Arc::clone(&p), Arc::clone(&q));
         println!("{expr}");
         assert_eq!(expr.to_string(), "∼(p ⋅ q)");
         let unnamed_space = UnnamedGen::new();
@@ -86,7 +102,7 @@ mod tests {
     fn test_dm_or_not() {
         let p = named_var_expr("p");
         let q = named_var_expr("q");
-        let expr = Arc::new(two_or_not(Arc::clone(&p), Arc::clone(&q)));
+        let expr = two_or_not(Arc::clone(&p), Arc::clone(&q));
         println!("{expr}");
         assert_eq!(expr.to_string(), "∼p ∨ ∼q");
         let unnamed_space = UnnamedGen::new();
@@ -99,7 +115,7 @@ mod tests {
     fn test_dm_not_or() {
         let p = named_var_expr("p");
         let q = named_var_expr("q");
-        let expr = Arc::new(two_not_or(Arc::clone(&p), Arc::clone(&q)));
+        let expr = two_not_or(Arc::clone(&p), Arc::clone(&q));
         println!("{expr}");
         assert_eq!(expr.to_string(), "∼(p ∨ q)");
         let unnamed_space = UnnamedGen::new();
@@ -112,7 +128,7 @@ mod tests {
     fn test_dm_and_not() {
         let p = named_var_expr("p");
         let q = named_var_expr("q");
-        let expr = Arc::new(two_and_not(Arc::clone(&p), Arc::clone(&q)));
+        let expr = two_and_not(Arc::clone(&p), Arc::clone(&q));
         println!("{expr}");
         assert_eq!(expr.to_string(), "∼p ⋅ ∼q");
         let unnamed_space = UnnamedGen::new();
@@ -125,7 +141,7 @@ mod tests {
     fn test_com_or() {
         let p = named_var_expr("p");
         let q = named_var_expr("q");
-        let expr = Arc::new(or(Arc::clone(&p), Arc::clone(&q)));
+        let expr = or(Arc::clone(&p), Arc::clone(&q));
         println!("{expr}");
         assert_eq!(expr.to_string(), "p ∨ q");
         let unnamed_space = UnnamedGen::new();
@@ -138,7 +154,7 @@ mod tests {
     fn test_com_and() {
         let p = named_var_expr("p");
         let q = named_var_expr("q");
-        let expr = Arc::new(and(Arc::clone(&p), Arc::clone(&q)));
+        let expr = and(Arc::clone(&p), Arc::clone(&q));
         println!("{expr}");
         assert_eq!(expr.to_string(), "p ⋅ q");
         let unnamed_space = UnnamedGen::new();
