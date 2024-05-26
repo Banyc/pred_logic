71fbdd128a45310f95e28f50a2dff77fcd19a45d
Banyc <36535895+Banyc@users.noreply.github.com>
2024-05-26 21:02:14 +0800

    feat: Simp

--- src/nat_deduct.rs
+++ src/nat_deduct.rs
@@ -6,11 +6,11 @@ use crate::{
 };
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Syllogism {
-    pub major_prem: Arc<Expr>,
-    pub minor_prem: Arc<Expr>,
+pub struct Syllogism<'a> {
+    pub major_prem: &'a Arc<Expr>,
+    pub minor_prem: &'a Arc<Expr>,
 }
-impl Syllogism {
+impl Syllogism<'_> {
     pub fn modus_ponens(&self, unnamed_space: &UnnamedGen) -> Option<Arc<Expr>> {
         let mut unnamed_space = unnamed_space.clone();
         let p = Var::Unnamed(unnamed_space.gen());
@@ -192,10 +192,26 @@ impl Syllogism {
     }
 
     fn extract(&self, major_pattern: &Expr, minor_patter: &Expr) -> Option<VarExprMap> {
-        let captured_1 = extract(&self.major_prem, major_pattern)?;
-        let captured_2 = extract(&self.minor_prem, minor_patter)?;
+        let captured_1 = extract(self.major_prem, major_pattern)?;
+        let captured_2 = extract(self.minor_prem, minor_patter)?;
         merge(captured_1, captured_2)
     }
+}
+
+pub fn simplification(prem: &Arc<Expr>, unnamed_space: &UnnamedGen) -> Option<Arc<Expr>> {
+    let mut unnamed_space = unnamed_space.clone();
+    let p = Var::Unnamed(unnamed_space.gen());
+    let q = Var::Unnamed(unnamed_space.gen());
+    let p_expr = Arc::new(Expr::Var(p.clone()));
+    let q_expr = Arc::new(Expr::Var(q.clone()));
+    let pat = Expr::BinOp(BinOpExpr {
+        op: BinOp::And,
+        left: Arc::clone(&p_expr),
+        right: Arc::clone(&q_expr),
+    });
+    let captured = extract(prem, &pat)?;
+    let conclusion = captured.get(&p).unwrap();
+    Some(Arc::clone(conclusion))
 }
 
 #[cfg(test)]
@@ -217,8 +233,8 @@ mod tests {
         println!("{major_prem}");
         println!("{minor_prem}");
         let syllogism = Syllogism {
-            major_prem,
-            minor_prem,
+            major_prem: &major_prem,
+            minor_prem: &minor_prem,
         };
         let unnamed_space = UnnamedGen::new();
         let conclusion = syllogism.modus_ponens(&unnamed_space).unwrap();
@@ -242,8 +258,8 @@ mod tests {
         println!("{major_prem}");
         println!("{minor_prem}");
         let syllogism = Syllogism {
-            major_prem,
-            minor_prem,
+            major_prem: &major_prem,
+            minor_prem: &minor_prem,
         };
         let unnamed_space = UnnamedGen::new();
         let conclusion = syllogism.modus_tollens(&unnamed_space).unwrap();
@@ -275,8 +291,8 @@ mod tests {
         println!("{major_prem}");
         println!("{minor_prem}");
         let syllogism = Syllogism {
-            major_prem,
-            minor_prem,
+            major_prem: &major_prem,
+            minor_prem: &minor_prem,
         };
         let unnamed_space = UnnamedGen::new();
         let conclusion = syllogism
@@ -309,8 +325,8 @@ mod tests {
         println!("{major_prem}");
         println!("{minor_prem}");
         let syllogism = Syllogism {
-            major_prem,
-            minor_prem,
+            major_prem: &major_prem,
+            minor_prem: &minor_prem,
         };
         let unnamed_space = UnnamedGen::new();
         let conclusion = syllogism.disjunctive_syllogism(&unnamed_space).unwrap();
@@ -345,8 +361,8 @@ mod tests {
         println!("{major_prem}");
         println!("{minor_prem}");
         let syllogism = Syllogism {
-            major_prem,
-            minor_prem,
+            major_prem: &major_prem,
+            minor_prem: &minor_prem,
         };
         let unnamed_space = UnnamedGen::new();
         let conclusion = syllogism.conjunctive_dilemma(&unnamed_space).unwrap();
@@ -394,8 +410,8 @@ mod tests {
         println!("{major_prem}");
         println!("{minor_prem}");
         let syllogism = Syllogism {
-            major_prem,
-            minor_prem,
+            major_prem: &major_prem,
+            minor_prem: &minor_prem,
         };
         let unnamed_space = UnnamedGen::new();
         let conclusion = syllogism.disjunctive_dilemma(&unnamed_space).unwrap();
@@ -414,6 +430,22 @@ mod tests {
                 })),
             })
         );
+    }
+
+    #[test]
+    fn test_simp() {
+        let p = named_var_expr("p");
+        let q = named_var_expr("q");
+        let prem = Arc::new(Expr::BinOp(BinOpExpr {
+            op: BinOp::And,
+            left: Arc::clone(&p),
+            right: Arc::clone(&q),
+        }));
+        println!("{prem}");
+        let unnamed_space = UnnamedGen::new();
+        let conclusion = simplification(&prem, &unnamed_space).unwrap();
+        println!("{conclusion}");
+        assert_eq!(conclusion, p);
     }
 
     fn named_var_expr(name: &str) -> Arc<Expr> {
